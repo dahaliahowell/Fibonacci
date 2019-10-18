f7eae67dee7a4d3de198d12333751f4180ad8006
Dahalia Howell <dahaliahowell.dh@gmail.com>
2019-10-18 00:03:46 -0500

    My First Commit

--- fib.py
+++ fib.py
@@ -1,3 +1,9 @@
+#Name:  Dahalia Howell
+
+#My First Ever Github Project :)
+
+#This function checks an inputted list to determine whether or not it is a fibonacci sequence.
+#It returns True if it is and False if it is not.
 def fib_check(lst):
     target = lst[0]
     counter = 0
@@ -7,7 +13,7 @@ def fib_check(lst):
         return False
     return lst == genFib(counter, counter + len(lst))
 
-
+#This function returns the fibonacci number of its input.
 def fib(x):
     if x == 0:
         return 0
@@ -16,7 +22,9 @@ def fib(x):
     else:
         return fib(x-1) + fib(x-2)
 
-
+#This function accepts the starting number and one plus the ending number of a range as input.
+#It the finds the fibonacci number of each number within the range and adds them to a list after each iteration.
+#The function returns a list containing the fibonacci sequence of the range.
 def genFib(start, end):
     lst = []
     for i in range(start, end):
